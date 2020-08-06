0c56f6b92a73c183e2793230a054b65ae5c98ed6
kston <kenestonsc@gmail.com>
2020-08-06 15:36:19 -0300

    120.w09

--- src/controller/AppController.js
+++ src/controller/AppController.js
@@ -139,19 +139,90 @@ class AppController {
     });
 
     this.el.btnAttachPhoto.on('click', (e) => {
-      console.log('photo');
+      this.el.inputPhoto.click();
     });
+
+    this.el.inputPhoto.on('change', (e) => {
+      [...this.el.inputPhoto.files].forEach((file) => {
+        console.log(file);
+      });
+    });
+
     this.el.btnAttachCamera.on('click', (e) => {
-      console.log('Camera');
+      this.closeAllMainPanel();
+      this.el.panelCamera.addClass('open');
+      this.el.panelCamera.css({
+        height: 'calc(100% - 120px)',
+      });
+    });
+
+    this.el.btnClosePanelCamera.on('click', (e) => {
+      this.closeAllMainPanel();
+      this.el.panelMessagesContainer.show();
+    });
+
+    this.el.btnTakePicture.on('click', (e) => {
+      alert('omg!');
     });
 
     this.el.btnAttachDocument.on('click', (e) => {
-      console.log('document');
+      this.closeAllMainPanel();
+      this.el.panelDocumentPreview.addClass('open');
+      this.el.panelDocumentPreview.css({
+        height: 'calc(100% - 120px)',
+      });
+    });
+
+    this.el.btnClosePanelDocumentPreview.on('click', (e) => {
+      this.closeAllMainPanel();
+      this.el.panelMessagesContainer.show();
+    });
+
+    this.el.btnSendDocument.on('click', (e) => {
+      console.log('oi');
     });
 
     this.el.btnAttachContact.on('click', (e) => {
-      console.log('contact');
+      this.el.modalContacts.show();
     });
+
+    this.el.btnCloseModalContacts.on('click', (e) => {
+      this.el.modalContacts.hide();
+    });
+
+    this.el.btnSendMicrophone.on('click', (e) => {
+      this.el.recordMicrophone.show();
+      this.el.btnSendMicrophone.hide();
+      this.startRecordMicrophoneTime();
+    });
+
+    this.el.btnCancelMicrophone.on('click', (e) => {
+      this.closeRecordMicrophone();
+    });
+
+    this.el.btnFinishMicrophone.on('click', (e) => {
+      this.closeRecordMicrophone();
+    });
+  }
+
+  startRecordMicrophoneTime() {
+    let start = Date.now();
+
+    this._recordMicrophoneInterval = setInterval(() => {
+      this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+    }, 100);
+  }
+
+  closeRecordMicrophone() {
+    this.el.recordMicrophone.hide();
+    this.el.btnSendMicrophone.show();
+    clearInterval(this._recordMicrophoneInterval);
+  }
+
+  closeAllMainPanel() {
+    this.el.panelMessagesContainer.hide();
+    this.el.panelCamera.removeClass('open');
+    this.el.panelDocumentPreview.removeClass('open');
   }
 
   closeMenuAttach(e) {
