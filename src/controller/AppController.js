cd8f4293243034ee7f6d9fbd857ac425e60bf1e6
kston <kenestonsc@gmail.com>
2020-08-06 10:42:22 -0300

    New events

--- src/controller/AppController.js
+++ src/controller/AppController.js
@@ -1,7 +1,5 @@
 class AppController {
   constructor() {
-    console.log('App is working!');
-
     this.elementsPrototype();
     this.loadElements();
     this.initEvents();
@@ -64,6 +62,18 @@ class AppController {
     Element.prototype.hasClass = function (name) {
       return this.classList.contains(name);
     };
+
+    HTMLFormElement.prototype.getForm = function () {
+      return new FormData(this);
+    };
+
+    HTMLFormElement.prototype.toJSON = function () {
+      let json = {};
+      this.getForm().forEach((value, key) => {
+        json[key] = value;
+      });
+      return json;
+    };
   }
   initEvents() {
     this.el.myPhoto.on('click', (e) => {
@@ -90,6 +100,63 @@ class AppController {
     this.el.btnClosePanelAddContact.on('click', (e) => {
       this.el.panelAddContact.removeClass('open');
     });
+
+    this.el.photoContainerEditProfile.on('click', (e) => {
+      this.el.inputProfilePhoto.click();
+    });
+
+    this.el.inputNamePanelEditProfile.on('keypress', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+
+        this.el.btnSavePanelEditProfile.click();
+      }
+    });
+
+    this.el.btnSavePanelEditProfile.on('click', (e) => {
+      console.log(this.el.inputNamePanelEditProfile.innerHTML);
+    });
+
+    this.el.formPanelAddContact.on('submit', (e) => {
+      e.preventDefault();
+
+      let formData = new FormData(this.el.formPanelAddContact);
+    });
+
+    this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach((item) => {
+      item.on('click', (e) => {
+        this.el.home.hide();
+        this.el.main.css({
+          display: 'flex',
+        });
+      });
+    });
+
+    this.el.btnAttach.on('click', (e) => {
+      e.stopPropagation();
+      this.el.menuAttach.addClass('open');
+      document.addEventListener('click', this.closeMenuAttach.bind(this));
+    });
+
+    this.el.btnAttachPhoto.on('click', (e) => {
+      console.log('photo');
+    });
+    this.el.btnAttachCamera.on('click', (e) => {
+      console.log('Camera');
+    });
+
+    this.el.btnAttachDocument.on('click', (e) => {
+      console.log('document');
+    });
+
+    this.el.btnAttachContact.on('click', (e) => {
+      console.log('contact');
+    });
+  }
+
+  closeMenuAttach(e) {
+    document.removeEventListener('click', this.closeMenuAttach);
+    this.el.menuAttach.removeClass('open');
   }
 
   closeAllLeftPanel() {
